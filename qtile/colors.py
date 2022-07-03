f3dde6f02482f564beb8fac758628096c6551444
NationalNeedleMouse <87540491+NNeedlemouse@users.noreply.github.com>
2022-07-03 11:23:15 -0500

    Add files via upload

---
+++ qtile/colors.py
@@ -0,0 +1,14 @@
+colors = {
+    "background": '#181818',
+    "current_line": '#282828',
+    "foreground": '#f8f8f2',
+    "comment": '#535453',
+    "black":   '#181818',
+    "red":     '#ab4642',
+    "green":   '#a1b56c',
+    "yellow":  '#f7ca88',
+    "blue":    '#7cafc2',
+    "magenta": '#ba8baf',
+    "cyan":    '#86c1b9',
+    "white":   '#d8d8d8',
+}
