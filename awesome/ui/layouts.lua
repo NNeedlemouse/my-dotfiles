88f53bd650ddf93e5b7486b214b8b0c9b32b8b54
NationalNeedleMouse <87540491+NNeedlemouse@users.noreply.github.com>
2022-07-12 23:11:11 -0500

    Delete awesome directory

--- awesome/ui/layouts.lua
+++
@@ -1,8 +0,0 @@
-local gears = require("gears")
-local awful = require("awful")
-local beautiful = require("beautiful")
-
-awful.layout.layouts = {
-    awful.layout.suit.tile,
-    awful.layout.suit.floating,
-}
