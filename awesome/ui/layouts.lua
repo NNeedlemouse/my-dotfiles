6e0a37b867a6a8f22ee777eed62079e431b6ee08
NationalNeedleMouse <87540491+NNeedlemouse@users.noreply.github.com>
2022-07-12 20:49:43 -0500

    Add files via upload

---
+++ awesome/ui/layouts.lua
@@ -0,0 +1,8 @@
+local gears = require("gears")
+local awful = require("awful")
+local beautiful = require("beautiful")
+
+awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.floating,
+}
